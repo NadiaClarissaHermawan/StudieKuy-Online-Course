bd5f8f17c2c57877367f1fe4957444c8277f022d
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-11 08:03:11 +0700

    Update contoh.java

--- contoh.java
+++ contoh.java
@@ -1,6 +1,11 @@
 public class contoh{
     public static void main(String[] args){
+<<<<<<< HEAD
+        System.out.println("Hello World")
+=======
         System.out.println("Hello World");
         System.out.println();
+<<<<<<< HEAD
+        //tester
     }
 }
