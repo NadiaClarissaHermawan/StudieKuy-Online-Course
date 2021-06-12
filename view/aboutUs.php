6c48249698dfc1ed110397c505470a3331b912f5
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-12 21:00:49 +0700

    visi misi about us

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -35,14 +35,20 @@
             <hr class="hr1">
         </div>
         <div class="inline" style="vertical-align:middle;">
-            <img src="view/images/aboutUs2.png" id="gambarIndex1"/> 
-        </div>
-        <div class="inline" style="vertical-align:middle;">
-            <div class="tulisanPutih" id="about-us">
-                Perkembangan teknologi yang cukup pesat saat ini telah banyak berperan dalam kehidupan masyarkat sehari-hari khususnya dalam bidang pendidikan. Generasi milenial saat ini tentunya sudah tidak asing dengan sistem pembelajaran secara daring.
-        
-                OnlineCourse.com merupakan salah satu platform kursus online yang didirikan dengan tujuan untuk membantu masyarakat untuk dapat memperoleh ilmu baru yang menarik yang disuguhkan oleh tenaga-tenaga pendidik yang profesional.
+            <div class="tulisanCoklat" id="visi">
+                <ol>Visi :
+                    <p>Menjadi platform online course terbaik nasional, dengan metode yang modern, dan sistem yang terintegrasi.</p>
+                </ol>
             </div>
+            <div class="tulisanCoklat" id="misi">
+                <ol>Misi :
+                    <li>Menyelenggarakan program belajar online yang memenuhi harapan user dengan menerapkan metode modern, dan sistem yang terintegrasi.</li>
+                    <li>Menjadi platform Online Course yang terus berinovasi dan menyesuaikan dengan kemajuan jaman.</li>
+                </ol>
+            </div>
+        </div>
+        <div class="inline" style="vertical-align:middle; display: inline-block;">
+            <img src="view/images/aboutUs2.png" id="gambarIndex2"/> 
         </div>
     </div>
 </div>
