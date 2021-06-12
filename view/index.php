427f061c0d0ad0a7eef6ce2bb050383ecb07d795
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-12 19:53:33 +0700

    update index.php egein :)

--- view/index.php
+++ view/index.php
@@ -1,4 +1,4 @@
-<div class="main">
+<div class="main" style="height: 1500px;">
     <div class="nav fix">
         <a href="courses" class="menuNav">Courses</a>
         <a href="aboutUs" class="menuNav">About Us</a>
@@ -7,24 +7,38 @@
     </div>
 
     <!-- blok coklat paling atas  -->
-    <div style="height: 503px;">
-        <div class="inline">
+    <div style="height: 480px">
+        <div class="inline" style="vertical-align:middle;">
             <div class="tulisanPutih" id="learnNewKnowledge">
                 Learn new knowledge and skills in a variety of ways, 
                 from engaging video lectures and dynamic graphics to
                 data visualizations and interactive element.
             </div>
-            <button type="submit" id="tombolStartLearning" style="font-size: 24px;">Start Learning Now!</button>
+            <a href="courses"><button type="submit" id="tombolStartLearning" style="font-size: 24px;">Start Learning Now!</button></a>
         </div>  
-        <div class="inline">
+        <div class="inline" style="vertical-align:middle;">
             <img src="view/images/mainPage1.png" id="gambarIndex1"/> 
         </div>    
     </div>
-    
+
     <!-- blok pink tengah -->
-    <div  class="inline pinkbgIdx">
-        <div>
+    <div  id="pinkbgIdx">
+        <div class="inline" style="vertical-align:middle;">
             <img src="view/images/mainPage2.png" id="gambarIndex2"/>
+        </div>
+        <div class="inline" style="vertical-align:middle;">
+            <div class="tulisanPutih kotakCoklat">
+                FOR LEARNERS :
+                <br>
+                Propel your career, get a degree, or expand your knowledge at any level.
+                View all courses and programs   
+            </div>
+            <br>
+            <div class="tulisanPutih kotakCoklat">
+                FOR EDUCATORS :
+                <br>
+                Expand your curriculum through blended learning.
+            </div>
         </div>
     </div>
 
