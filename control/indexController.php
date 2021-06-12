6da56af3165b67993611466c069e64702be5fd0a
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-12 13:27:45 +0700

    bikin layout.php, indexcontroller

--- control/indexController.php
+++ control/indexController.php
@@ -1,8 +1,17 @@
 <?php 
     require_once "control/services/view.php";
+    require_once "control/services/mysqlDB.php";
 
-    class indexController(){
-        
+    class indexController{
+        protected $db;
+
+        public function __construct(){
+            $this->db = new MySQLDB("localhost", "root", "", "tubes");
+        }
+
+        public function view_mainpage(){
+            return View::createView('index.php', []);
+        }
     }
 
 ?>
---
+++ view/layout/layout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="View/layout/style/style.css"/>
+    </head>
+    <body>
+        <h1 id="judul"> Studie Kuy ! </h1>
+        
+        <!-- ini mesti ada buat keluarin konten yg uda dibikin sblmnya di php, kalo gaada, meski di echo di view jg gakan keluar -->
+        <?php echo $content; ?>
+    </body>
+</html>
