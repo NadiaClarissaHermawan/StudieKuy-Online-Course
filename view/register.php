535f4c9c946d391a942c12bc4f918eac5a023280
wei1001 <85715373+wei1001@users.noreply.github.com>
2021-06-12 15:59:43 +0700

    Update register.php

--- view/register.php
+++ view/register.php
@@ -1,5 +1,5 @@
 <div class="nav">
-    <a href="home"><div class="menuNav">Home</div></a>
+    <a href="index"><div class="menuNav">Home</div></a>
     <div class="menuNav">/</div>
     <div  class="menuNav">Register</div>
 </div>
