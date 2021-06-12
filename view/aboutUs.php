2cd90786f64744d986446a9d70f321b84aa8901a
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-12 19:12:55 +0700

    update css about us

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -5,16 +5,17 @@
     <a href="faq" class="menuNav">FAQ</a>
     <a href="myProfile" class="menuNav">My Profile</a>
 </div>
-<div class="content">
+<div class="content">  
     <hr class="hr1">
     <hr class="hr2">
     <div class="slideTitle" id="slide1">About Us</div>
     <hr class="hr2">
     <hr class="hr1">
+
+    <img src="view/images/aboutUs1.png">
     <div class="tulisanPutih" id="about-us">
         Perkembangan teknologi yang cukup pesat saat ini telah banyak berperan dalam kehidupan masyarkat sehari-hari khususnya dalam bidang pendidikan. Generasi milenial saat ini tentunya sudah tidak asing dengan sistem pembelajaran secara daring.
         
         OnlineCourse.com merupakan salah satu platform kursus online yang didirikan dengan tujuan untuk membantu masyarakat untuk dapat memperoleh ilmu baru yang menarik yang disuguhkan oleh tenaga-tenaga pendidik yang profesional.
     </div>
-    
 </div>
