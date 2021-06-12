66e5b3b3da6e7d9c008ab2f14ef492d4169eb6a9
wei1001 <85715373+wei1001@users.noreply.github.com>
2021-06-12 16:16:40 +0700

    add gambar sama register.php

--- view/register.php
+++ view/register.php
@@ -3,6 +3,36 @@
     <div class="menuNav">/</div>
     <div  class="menuNav">Register</div>
 </div>
-<div>
-	<img src="images/login.png">
-</div>
+<div class="form">
+	<img src="view/images/loginpotongan.png">
+	<form method="" action="">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="userID"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="Password" name="userPass"></td>
+			</tr>
+            <tr>
+				<td>Email</td>
+				<td><input type="text" name="userEmail"></td>
+			</tr>
+            <tr>
+				<td>Phone</td>
+				<td><input type="text" name="userPhone"></td>
+			</tr>
+            <tr>
+				<td>Role</td>
+				<td>
+                    <select name="roleSelector">
+                        <option value="1">--Select Role--</option>
+                        <option value="2">Student</option>
+                        <option value="3">Teacher</option>
+                    </select>
+                </td>
+			</tr>
+		</table>
+		<input type="submit" value="Register" name='updateUsername'>
+	</form>
