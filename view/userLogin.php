fd8ed83fc8fa8b2c234ad9c65e6d44363cce90d2
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-12 15:45:18 +0700

    update userlogin

--- view/userLogin.php
+++ view/userLogin.php
@@ -1,7 +1,22 @@
-<nav>
-	<a href="">Home / </a>
-	<a href="">Login</a>
-</nav>
+<div class="nav">
+    <a href="home"><div class="menuNav">Home /</div></a>
+    <a href=""><div class="menuNav">Login</div></a>
+</div>
 <div>
-	<img src="images/login.png">
+	<img src="images/login.jpg">
+	<form method="" action="">
+		<table>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="userID"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="Password" name="userPass"></td>
+			</tr>
+		</table>
+		<input type="submit" value="Login" name='updateUsername'>
+	</form>
+	<br>
+	<p>doesn't have an account?<a href="">Register Now!</a></p>
 </div>
