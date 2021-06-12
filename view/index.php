560639d001f7f291374e220b482e007ba9761ac6
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-12 15:22:27 +0700

    css class nav = blok putih navigasi, class navMenu = style untuk div dari tiap menunya

--- view/index.php
+++ view/index.php
@@ -1,6 +1,9 @@
 
 <div class="nav">
-    
+    <a href="courses"><div class="menuNav">Courses</div></a>
+    <a href="aboutUs"><div class="menuNav">About Us</div></a>
+    <a href="faq"><div  class="menuNav">FAQ</div></a>
+    <a href="myProfile"><div class="menuNav">My Profile</div></a>
 </div>
 <div class="tulisanPutih">
     Learn new knowledge and skills in a variety of ways, 
