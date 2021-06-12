446d6be9861f09b182437c7c5bd4fe88b64dc2ec
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-12 13:46:39 +0700

    header css

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -4,7 +4,10 @@
         <link rel="stylesheet" type="text/css" href="View/layout/style/style.css"/>
     </head>
     <body>
-        <h1 id="judul"> Studie Kuy ! </h1>
+    	<div class="header">
+    		<h1 id="judul"> Studie Kuy ! </h1>
+    	</div>
+        <nav></nav>
         
         <!-- ini mesti ada buat keluarin konten yg uda dibikin sblmnya di php, kalo gaada, meski di echo di view jg gakan keluar -->
         <?php echo $content; ?>
