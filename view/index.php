f1182aaee4e262f72abc23ea974f25b6d1a1724a
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-12 18:42:12 +0700

    fix idx.php

--- view/index.php
+++ view/index.php
@@ -1,24 +1,32 @@
+<div class="main">
+    <div class="nav fix">
+        <a href="courses" class="menuNav">Courses</a>
+        <a href="aboutUs" class="menuNav">About Us</a>
+        <a href="faq" class="menuNav">FAQ</a>
+        <a href="myProfile" class="menuNav">My Profile</a>
+    </div>
 
-<div class="nav fix">
-    <a href="courses" class="menuNav">Courses</a>
-    <a href="aboutUs" class="menuNav">About Us</a>
-    <a href="faq" class="menuNav">FAQ</a>
-    <a href="myProfile" class="menuNav">My Profile</a>
-</div>
-<div>
-    <div class="tulisanPutih" id="learnNewKnowledge">
-        Learn new knowledge and skills in a variety of ways, 
-        from engaging video lectures and dynamic graphics to
-        data visualizations and interactive element.
+    <!-- blok coklat paling atas  -->
+    <div style="height: 503px;">
+        <div class="inline">
+            <div class="tulisanPutih" id="learnNewKnowledge">
+                Learn new knowledge and skills in a variety of ways, 
+                from engaging video lectures and dynamic graphics to
+                data visualizations and interactive element.
+            </div>
+            <button type="submit" id="tombolStartLearning" style="font-size: 24px;">Start Learning Now!</button>
+        </div>  
+        <div class="inline">
+            <img src="view/images/mainPage1.png" id="gambarIndex1"/> 
+        </div>    
     </div>
-    <div>
-        <button type="submit" id="tombolStartLearning"><div class="tulisanCoklat" style="font-size: 24px;">Start Learning Now !</div></button>
-    </div>
-    <div>
-        <img src="view/images/mainPage1.png" id="gambarIndex1"/>
-        <div class="tulisanPutih" id="quoteMainPage">
-            “The single most important key to success is to be a good listener.”
-            ― Kelly Wearstler   
+    
+    <!-- blok pink tengah -->
+    <div  class="inline pinkbgIdx">
+        <div>
+            <img src="view/images/mainPage2.png" id="gambarIndex2"/>
         </div>
     </div>
+
+
 </div>
