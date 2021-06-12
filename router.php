f443a9b9ddd73c6486fd6a4907d943881f097180
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-12 12:59:25 +0700

    bikin base router.php

---
+++ router.php
@@ -0,0 +1,11 @@
+<?php 
+    $url = $_SERVER['REDIRECT_URL'];
+    $baseURL = '/TugasBesar';
+
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        switch($url){
+            
+        }
+    }
+
+?>
