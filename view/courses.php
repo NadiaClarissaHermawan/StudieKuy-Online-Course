660cc03a41ee0fc4297aa8a8c7b7683f76569439
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-13 19:40:51 +0700

    update css course

--- view/courses.php
+++ view/courses.php
@@ -4,9 +4,30 @@
     <a href="faq" class="menuNav">FAQ</a> 
 </div>
 
-<div class="allMid" id="contentMainPage">
-    <div class="allMid" id="line1">
-        <div class="courses1">
+<div class="allMid col" id="contentMainPage">
+    <div class="allMid" id="col1">
+        <img src="view/images/course1.png" id="course1-image">
+        <img src="view/images/course4.png" id="course4-image">        
+    </div>
+
+    <div class="allMid col" id="col2">
+        <img src="view/images/course2.png" id="course2-image">
+        <img src="view/images/course5.jpg" id="course5-image">
+    </div>
+    <div class="allMid col" id="col3">
+        <img src="view/images/course3.png" id="course3-image">
+        <img src="view/images/course6.png" id="course6-image">
+    </div>
+</div>
+
+
+<!-- <div class="allMid" id="contentMainPage">
+    <div class="allMid" id="col1">
+        <img src="view/images/course1.png" id="course1-image">
+        <img src="view/images/course2.png" id="course2-image">
+        <img src="view/images/course3.png" id="course3-image"> -->
+
+        <!-- <div class="courses1">
             <img src="view/images/course1.png" id="course1-image">
         </div>
         <div class="courses2">
@@ -14,10 +35,15 @@
         </div>
         <div class="courses3">
         	<img src="view/images/course3.png" id="course3-image">
-        </div>
-    </div>
-    <div class="allMid" id="line1">
-        <div class="courses4">
+        </div> -->
+    <!-- </div>
+
+    <div class="allMid" id="line2">
+        <img src="view/images/course4.png" id="course4-image">
+        <img src="view/images/course5.jpg" id="course5-image">
+        <img src="view/images/course6.png" id="course6-image"> -->
+
+        <!-- <div class="courses4">
             <img src="view/images/course4.png" id="course4-image">
         </div>
         <div class="courses5">
@@ -25,6 +51,6 @@
         </div>
         <div class="courses6">
         	<img src="view/images/course6.png" id="course6-image">
-        </div>
-    </div>
-</div>
+        </div> -->
+<!--     </div>
+</div> -->
