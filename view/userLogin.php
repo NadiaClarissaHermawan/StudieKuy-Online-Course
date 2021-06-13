2482513f890b7f683b16ef795d8d8091bba1e2c5
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-13 17:28:51 +0700

    register.php done

--- view/userLogin.php
+++ view/userLogin.php
@@ -12,12 +12,12 @@
             <div class="rowLogin tulisanCoklat">
 				<label for="uname" class="txt hurufSedang">Username</label>
 				<span style="width: 5px;" class="hurufSedang">:</span>
-                <input type="text" class="kotakInput hurufSedang" id="uname" name="uname" placeholder="Enter Username"/>
+                <input type="text" class="kotakInput hurufSedang" id="uname" name="uname" placeholder="Enter username"/>
             </div>
             <div class="rowLogin tulisanCoklat" style="margin-bottom: 30px;">
 				<label for="upass" class="txt hurufSedang">Password</label>
 				<span style="width: 5px;"  class="hurufSedang">:</span>
-                <input type="password" class="kotakInput hurufSedang" id="upass" name="upass" placeholder="Enter Password"/>
+                <input type="password" class="kotakInput hurufSedang" id="upass" name="upass" placeholder="Enter password"/>
             </div>
             <div class="rowLogin tulisanCoklat" style="margin-bottom: 10px;">
 				<a href="index" style="text-decoration: none;"><button type="submit" class="button link tulisanCoklat">Log in</button></a>
--- view/userRegister.php
+++ view/userRegister.php
@@ -11,20 +11,26 @@
         <div class="contentLogin">
             <div class="rowLogin tulisanCoklat">
 				<label for="uname" class="txt hurufSedang">Username</label>
-				<span style="width: 5px;" class="hurufSedang">:</span>
-                <input type="text" class="kotakInput hurufSedang" id="uname" name="uname" placeholder="Enter Username"/>
-            </div>
-            <div class="rowLogin tulisanCoklat" style="margin-bottom: 30px;">
-				<label for="upass" class="txt hurufSedang">Password</label>
-				<span style="width: 5px;"  class="hurufSedang">:</span>
-                <input type="password" class="kotakInput hurufSedang" id="upass" name="upass" placeholder="Enter Password"/>
-            </div>
-            <div class="rowLogin tulisanCoklat" style="margin-bottom: 10px;">
-				<a href="index" style="text-decoration: none;"><button type="submit" class="button link tulisanCoklat">Log in</button></a>
+				<span style="width: 7px;" class="hurufSedang">:</span>
+                <input type="text" class="kotakInput hurufSedang" id="uname" name="uname" placeholder="Enter username"/>
             </div>
             <div class="rowLogin tulisanCoklat">
-                <span class="hurufKecil">doesn't have an account? </span>
-                <a class="link hurufKecil" href="userRegister"> Register now!</a>
+				<label for="upass" class="txt hurufSedang">Password</label>
+				<span style="width: 7px;"  class="hurufSedang">:</span>
+                <input type="password" class="kotakInput hurufSedang" id="upass" name="upass" placeholder="Enter password"/>
+            </div>
+			<div class="rowLogin tulisanCoklat">
+				<label for="uemail" class="txt hurufSedang">E-mail</label>
+				<span style="width: 7px;"  class="hurufSedang">:</span>
+                <input type="password" class="kotakInput hurufSedang" id="uemail" name="uemail" placeholder="Enter e-mail"/>
+            </div>
+			<div class="rowLogin tulisanCoklat" style="margin-bottom: 30px;">
+				<label for="uphone" class="txt hurufSedang">Phone</label>
+				<span style="width: 7px;"  class="hurufSedang">:</span>
+                <input type="password" class="kotakInput hurufSedang" id="uphone" name="uphone" placeholder="Enter phone number"/>
+            </div>
+            <div class="rowLogin tulisanCoklat" style="margin-bottom: 30px;">
+				<a href="index" style="text-decoration: none;"><button type="submit" class="button link tulisanCoklat">Register</button></a>
             </div>
         </div>
     </div>
