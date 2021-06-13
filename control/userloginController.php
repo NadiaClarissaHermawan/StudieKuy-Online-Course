8e1b4f361d78e15bc7630e6ecbec1b77e743e177
ClarissaNadiaa <6181901013@student.unpar.ac.id>
2021-06-13 20:42:52 +0700

    update userlogincontroller.php

--- control/userloginController.php
+++ control/userloginController.php
@@ -14,7 +14,31 @@
         }
 
         public function klik_login(){
-            
+            $uname = $_POST['uname'];
+            $upass = $_POST['upass'];
+
+            if(isset($uname) && $uname!="" && isset($upass) && $uname!=""){
+                $uname = $this->db->escapeString($uname);
+                $upass = $this->db->escapeString($upass);
+
+                $query = "SELECT password FROM pengguna WHERE nama_user = '$uname'";
+                $pass_asli = $this->db->executeSelectQuery($query);
+
+                //kalo username tdk tercantum di database
+                if(empty($pass_asli)){
+                    //javascript ngasih tau akun tidak ditemukan
+               
+                }else{
+                    //password benar
+                    if($upass == $pass_asli){
+                        header('Location: index');
+                        
+                    //password salah
+                    }else{
+                        //javascript ngasih tau password salah
+                    }
+                }
+            }
         }
     }
 ?>
