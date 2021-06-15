ded9527f249c3fff2572cbfc485a4ca1055d39bd
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-15 15:15:18 +0700

    fix layoutUserProfile yg conflict

--- view/layout/layoutUserProfile.php
+++ view/layout/layoutUserProfile.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"> 
         <link rel="stylesheet" type="text/css" href="view/layout/style/userProfile.css"/>
         <link rel="stylesheet" type="text/css" href="view/layout/style/header.css"/>
         <link rel="stylesheet" type="text/css" href="view/layout/style/globalNeeds.css"/>
@@ -11,8 +12,8 @@
     		<h1 id="judul" style="display: inline-block;"> Studie Kuy ! </h1>
 
             <?php 
-                // $saldoUser = $_SESSION['saldo'];
-                // echo '<a href="userTopup"><button type="submit" name="topupButton" class="tulisanCoklat" id="header-topupButton">'.$saldoUser.'</button></a>';
+                $saldoUser = $_SESSION['saldo'];
+                echo '<a href="userTopup"><button type="submit" name="topupButton" class="tulisanCoklat" id="header-topupButton">'.$saldoUser.'</button></a>';
             ?>
     	</div>
         
@@ -26,6 +27,9 @@
                     echo ' <a href="userProfile" class="menuNavNow">My Profile</a>';
                 }
             ?>
+            <div class="menuNavKanan">
+                <a href="index" class="material-icons md-36">home</a>
+            </div>
         </div>
         
         <!-- ini mesti ada buat keluarin konten yg uda dibikin sblmnya di php, kalo gaada, meski di echo di view jg gakan keluar -->
