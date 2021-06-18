cb7c7703e1a8ba0ba95c7a9d44aef2b256076f83
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-18 11:07:28 +0700

    Design sementara menu verifikasi

--- view/verifikasiAdmin.php
+++ view/verifikasiAdmin.php
@@ -1,3 +1,13 @@
 <div id="contentMainPage">
-    
+    <div class="content2">
+        <div class="content2-kiri">
+            <a href="">Sertificate Verification</a>
+        </div>
+        <div class="content2-tengah">
+            <a href="">Course Transaction Verification</a>
+        </div>
+        <div class="content2-kanan">
+            <a href="">Top-Up Transaction Verification</a>
+        </div>
+    </div>
 </div>
