f53ec8aee78c02b939ceadf94126b4879ae18822
wei1001 <85715373+wei1001@users.noreply.github.com>
2021-06-18 10:08:42 +0700

    benerin faq

--- view/faq.php
+++ view/faq.php
@@ -1,36 +1,56 @@
 <div class="container">
     <div class="container-1">
         <div class="talk-bubble tri-right round left-in">
-        <div class="talktext question">
-            <img src="view/images/ppTasha.jpg" class="profilePic" style="margin-right: 10px;">
-            <p>Moving our way back up the right side indented. Uses .round and .right-in</p>
-        </div>
+            <div class="talktext question">
+                <img src="view/images/ppTasha.jpg" class="profilePic" style="margin-right: 10px;">
+                <br>
+                <p>Mengapa bumi bulat?</p>
+                <!-- <br> -->
+            </div>
         </div>
         <div class="talk-bubble tri-right round right-in " style="margin-left: 44%;">
-        <div class="talktext answer">
-            <img src="view/images/profpicAdmin.jpg" class="profilePic">
-            <p>Moving our way back up the right side indented. Uses .round and .right-in</p>
-        </div>
+            <div class="talktext answer">
+                <img src="view/images/profpicAdmin.jpg" class="profilePic">
+                <p>Karena saat kita berjalan lurus dari suatu tempat maka akan kembali ke tempat itu.</p>
+            </div>
         </div>
     </div>
 
     <!-- lanjut container kedua  -->
     <div class="container-1" style="margin-top: 2%;">
         <div class="talk-bubble tri-right round left-in">
+            <div class="talktext question">
+                <img src="view/images/ppRio.png" class="profilePic" style="margin-right: 10px;">
+                <br>
+                <p>Apakah benar panda itu beruang?</p>
+            </div>
+        </div>
+        <div class="talk-bubble tri-right round right-in " style="margin-left: 44%;">
+            <div class="talktext answer">
+                <img src="view/images/profpicAdmin.jpg" class="profilePic">
+                <p>Ya, Panda merupakan salah satu spesies beruang.</p>
+            </div>
+        </div>
+    </div>
+    
+    <!-- lanjut container Ketiga -->
+    <div class="container-1" style="margin-top: 2%;">
+        <div class="talk-bubble tri-right round left-in">
         <div class="talktext question">
-            <img src="view/images/ppTasha.jpg" class="profilePic" style="margin-right: 10px;">
-            <p>Moving our way back up the right side indented. Uses .round and .right-in</p>
+            <img src="view/images/ppNadia.png" class="profilePic" style="margin-right: 10px;">
+            <br>
+            <p>Mengapa pohon tidak berjalan?</p>
         </div>
         </div>
         <div class="talk-bubble tri-right round right-in " style="margin-left: 44%;">
         <div class="talktext answer">
             <img src="view/images/profpicAdmin.jpg" class="profilePic">
-            <p>Moving our way back up the right side indented. Uses .round and .right-in</p>
+            <p>Pohon sebenarnya "bergerak", walau tidak bisa dilihat secara kasat mata.</p>
         </div>
         </div>
     </div>
-    
-    <div class="allMid">
+
+    <!-- <div class="allMid">
         <span class="talk-bubble tri-right round left-in" id="question" style="width: 40%;">
             <div class="talktext question">
                 <img src="view/images/profpic1.jpg" class="profilePic">
@@ -73,5 +93,5 @@
     </div>
     <div class="question2">
         
-    </div>
+    </div> -->
 </div>
