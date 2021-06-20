cbfc87e998340d6f95ba789d56621ccd55c9d69a
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-20 20:50:43 +0700

    courses-transaction

---
+++ view/course-transaction.php
@@ -0,0 +1,23 @@
+<div class="course-detail-main">
+    <div class="course-detail-top">
+        <h1>Java Basic Programming</h1>
+        <hr>
+    </div>
+    <div class="course-detail-isi">
+        <div class="course-detail-kiri">
+            <div>
+                <img src="/view/images/course2.png">
+            </div>
+            <button type="submit" class="course-detail-button" href="">
+                <h2 style="margin: 0;">Enroll Now for only<br> Rp. 50,000</h2>
+            </button>
+        </div>
+        <div class="course-detail-kanan">
+            <h2 style="margin: 0; font-size: 36px;">Java Basic Programming Course berisi :</h2>
+            <p style="font-size: 28px;">
+                1. Modul Data Types<br> 2. Modul IF-ELSE<br> 3. Modul WHILE/ FOR LOOPS<br> 4. Soal Latihan<br> 5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5.
+                Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir<br>5. Soal Ujian Akhir
+            </p>
+        </div>
+    </div>
+</div>
