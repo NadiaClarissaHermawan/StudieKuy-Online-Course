54e6c784717903b03a4910cbaf0e8ad3ed185223
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-20 15:05:01 +0700

    courses

--- view/courses.php
+++ view/courses.php
@@ -1,24 +1,30 @@
 <div id="contentMainPage">
     <div class="line1">
-        <div class="content2-kiri">
+        <div class="courses-card">
             <img src="view/images/courseee.png" id="course1-image" class="content-image one-edge-shadow row">
+            <h1>Computer</h1>
         </div>
-        <div class="content2-tengah">
+        <div class="courses-card">
             <img src="view/images/course2.png" id="course2-image" class="content-image one-edge-shadow row">
+            <h1>Economy</h1>
         </div>
-        <div class="content2-kanan">
+        <div class="courses-card">
             <img src="view/images/course3.png" id="course3-image" class="content-image one-edge-shadow">
+            <h1>Law</h1>
         </div>
     </div>
     <div class="line2">
-        <div class="content2-kiri">
+        <div class="courses-card">
             <img src="view/images/course4.png" id="course4-image" class="content-image one-edge-shadow">
+            <h1>Arts</h1>
         </div>
-        <div class="content2-tengah">
+        <div class="courses-card">
             <img src="view/images/course5.png" id="course5-image" class="content-image one-edge-shadow">
+            <h1>Science</h1>
         </div>
-        <div class="content2-kanan">
+        <div class="courses-card">
             <img src="view/images/course6.png" id="course6-image" class="content-image one-edge-shadow">
+            <h1>Language & Culture</h1>
         </div>
     </div>
 </div>
