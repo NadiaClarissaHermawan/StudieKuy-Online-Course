d8e0aded345cf0bedb7ce8a9d9e5115611c993f3
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-21 16:12:28 +0700

    teacher Course udh terhubung - tgl back end

---
+++ view/layout/layoutTeacherCourse.php
@@ -0,0 +1,32 @@
+<!-- terhubung ke :
+        viewIndexTeacher.php
+        indexTeacherController.php
+        teacherCourse.php
+        teacherCourse.css
+ -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="view/layout/style/globalNeeds.css"/>
+        <link rel="stylesheet" type="text/css" href="view/layout/style/header.css"/>
+        <link rel="stylesheet" type="text/css" href="view/layout/style/teacherCourse.css"/>
+    </head>
+    <body>
+        <div class="header">
+            <img src="view/images/logoStudieKuy.png" id="logo" style="display: inline-block;"/>
+            <h1 id="judul" style="display: inline-block;"> Teacher Studie Kuy !</h1>
+            <button name="loginButton" class="tulisanCoklat" id="header-loginButton">TEACHER</button>
+        </div>
+        
+        <div class="nav">
+            <a href="" class="menuNav">Courses List</a>
+            <div class="menuNavKanan">
+                <a href="indexTeacher" class="material-icons md-36">home</a>
+            </div>
+        </div>
+        
+        <!-- ini mesti ada buat keluarin konten yg uda dibikin sblmnya di php, kalo gaada, meski di echo di view jg gakan keluar -->
+        <?php echo $content; ?>
+    </body>
+</html>
--- view/teacherCourse.php
+++ view/teacherCourse.php
@@ -3,9 +3,9 @@
         <h1>TEACHER COURSE LIST</h1>
     </div>
     <div class="tcourse-isi">
-        <!-- yang ini ya yang di copy2 -->
+        yang ini ya yang di copy2
         <div class="tcourse-card">
-            <img src="course3.png">
+            <img src="view/images/course3.png">
             <div class="tcourse-card-info">
                 <h1>Java Basic Programming</h1>
             </div>
