1bd0f7cac94fb2afcd54a075560f6dd6e3b2b72d
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-23 23:29:56 +0700

    tampilan course modulllllll

--- view/courseModul.php
+++ view/courseModul.php
@@ -1,30 +1,42 @@
 <?php
-    if(session_status() == PHP_SESSION_NONE){
-        session_start();
-    }
+    // if(session_status() == PHP_SESSION_NONE){
+    //     session_start();
+    // }
 
-    // kalo belom login gabisa kesini
-    if(!isset($_SESSION['status'])){
-        header("Location: userLogin");
-        session_destroy();
-        exit;
-    }
+    // // kalo belom login gabisa kesini
+    // if(!isset($_SESSION['status'])){
+    //     header("Location: userLogin");
+    //     session_destroy();
+    //     exit;
+    // }
 ?>
-<div class="content1">
-    <div class="tulisanPutih hurufBesar"><?php echo $result[0]->getNamaCourse()?> Modules</div>
-</div>  
-<hr>
+<div>
+    <!-- <div class="navKiri">
+        <div class="menu">Modul 1</div>
+        <div class="menu">Modul 2</div>
+        <div class="menu">Modul 3</div>
+        <div class="menu">Modul 4</div>
+    </div> -->
+    <ul class="navKiri">
+        <li class="menu">Modul 1</li>
+        <li class="menu">Modul 2</li>
+        <li class="menu">Modul 3</li>
+    </ul>
+    <div class="vidShow tulisanCoklat hurufBesar">
+        //Video
+    </div>
+</div>
 
-<div class="white-box">
+<!-- <div class="white-box">
     <?php
-        if(isset($result) && $result!=null){
-            foreach($result as $key => $row){
-                echo '<a class="modul">';
-                echo '<i class="material-icons md-36" id="down">cloud_download</i>';
-                echo '<div class="text">Modul'.$row->getNamaModul().'</div>';
-                echo '</a>';
-            }
-        }
+        // if(isset($result) && $result!=null){
+        //     foreach($result as $key => $row){
+        //         echo '<a class="modul">';
+        //         echo '<i class="material-icons md-36" id="down">cloud_download</i>';
+        //         echo '<div class="text">Modul'.$row->getNamaModul().'</div>';
+        //         echo '</a>';
+        //     }
+        // }
     ?>
 
-</div>
+</div> -->
