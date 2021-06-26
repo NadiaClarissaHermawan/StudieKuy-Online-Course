92ffadefefbc0d10f2d18ec5e4b2ee0da76f615b
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-27 01:19:19 +0700

    edit warna + style chart report

--- view/chartCourse.php
+++ view/chartCourse.php
@@ -10,7 +10,7 @@
         exit;
     }
 ?>
-
+<div class="content1 hurufBesar tulisanPutih">Average Courses Achievements</div>
 <div class="chartReport" style="background-color: white;">
     <canvas id="myChart"></canvas>
 </div>
@@ -21,12 +21,20 @@
     let ch = document.getElementById('myChart').getContext('2d');
     let arrCourse = [];
     let arrAvgNilai = [];
+    let r,g,b;
+    let arrBackround = [];
+    let arrBorder = [];
 
     <?php
         foreach ($result as $key => $value) {
             ?>
             arrCourse.push('<?php echo $value['nama_course']?>');
             arrAvgNilai.push('<?php echo $value['rata2']?>');
+            r = Math.round(Math.random()*255);
+            g = Math.round(Math.random()*255);
+            b = Math.round(Math.random()*255);
+            arrBackround.push('rgba('+r+','+g+','+b+', 0.2)');
+            arrBorder.push('rgba('+r+','+g+','+b+', 1)');
             <?php
         }
     ?>
@@ -38,23 +46,9 @@
             datasets: [{
                 label: 'Average Score',
                 data: arrAvgNilai,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                backgroundColor: arrBackround,
+                borderColor: arrBorder,
+                borderWidth: 2
             }]
         },
         options: {
--- view/chartTopup.php
+++ view/chartTopup.php
@@ -10,7 +10,7 @@
         exit;
     }
 ?>
-
+<div class="content1 hurufBesar tulisanPutih">Total Incomes per Day</div>
 <div class="chartReport" style="background-color: white;">
     <canvas id="myChart"></canvas>
 </div>
@@ -21,40 +21,34 @@
     let ch = document.getElementById('myChart').getContext('2d');
     let arrTanggal = [];
     let arrJmlhTopup = [];
+    let r,g,b;
+    let arrBackround = [];
+    let arrBorder = [];
 
     <?php
         foreach ($result as $key => $value) {
             ?>
             arrTanggal.push('<?php echo $value['tanggal_transaksi_saldo']?>');
             arrJmlhTopup.push('<?php echo $value['total']?>');
+            r = Math.round(Math.random()*255);
+            g = Math.round(Math.random()*255);
+            b = Math.round(Math.random()*255);
+            arrBackround.push('rgba('+r+','+g+','+b+', 0.2)');
+            arrBorder.push('rgba('+r+','+g+','+b+', 1)');
             <?php
         }
     ?>
 
     let myChart = new Chart(ch, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: arrTanggal,
             datasets: [{
                 label: 'Total Income',
                 data: arrJmlhTopup,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                backgroundColor: arrBackround,
+                borderColor: arrBorder,
+                borderWidth: 2
             }]
         },
         options: {
