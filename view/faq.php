38cfb1cf651f13014a27a2492be7be9c60982a54
Tasha Boen <natashaBenedicta229@gmail.com>
2021-06-27 03:15:24 +0700

    buat template slideshow-> belom jalan!

--- view/faq.php
+++ view/faq.php
@@ -51,49 +51,4 @@
         </div>
         </div>
     </div>
-
-    <!-- <div class="allMid">
-        <span class="talk-bubble tri-right round left-in" id="question" style="width: 40%;">
-            <div class="talktext question">
-                <img src="view/images/profpic1.jpg" class="profilePic">
-                <p>Mengapa bumi bulat?</p>
-            </div>
-        </span>
-        <span class="talk-bubble tri-right round right-in" id="answer" style="width: 40%;">
-            <div class="talktext answer" >
-                <p>Karena tidak datar</p>
-                <img src="view/images/profpicAdmin.jpg" class="profilePic">
-            </div>
-        </span>
-    </div>
-    <div>
-        <div class="talk-bubble tri-right round left-in" id="question">
-            <div class="talktext question">
-                <img src="view/images/profpic1.jpg" class="profilePic">
-                <p>Apakah benar panda itu beruang?</p>
-            </div>
-        </div>
-        <div class="talk-bubble tri-right round right-in" id="answer">
-            <div class="talktext answer" >
-                <p>Ya, Panda merupakan salah satu spesies beruang</p>
-                <img src="view/images/profpicAdmin.jpg" class="profilePic">
-            </div>
-        </div>
-
-        <div class="talk-bubble tri-right round left-in" id="question">
-            <div class="talktext question">
-                <img src="view/images/profpic1.jpg" class="profilePic">
-                <p>Mengapa pohon tidak berjalan?</p>
-            </div>
-        </div>
-        <div class="talk-bubble tri-right round right-in" id="answer">
-            <div class="talktext answer" >
-                <p>Karena pohon tidak memiliki kaki.</p>
-                <img src="view/images/profpicAdmin.jpg" class="profilePic">
-            </div>
-        </div>
-    </div>
-    <div class="question2">
-        
-    </div> -->
 </div>
